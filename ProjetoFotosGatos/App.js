49b6c98d64ea18c9a82f09a61e49e6cb0cfddc61
Reinaldo1209 <reinaldofavorettosantana@gmail.com>
2024-11-22 00:42:56 -0300

    feat(p1-pdmn): cria função obter fotos

--- ProjetoFotosGatos/App.js
+++ ProjetoFotosGatos/App.js
@@ -4,10 +4,20 @@ import { StyleSheet, Text, View, Button, ScrollView, Image } from 'react-native'
 
 export default function App() {
 
+  const obterFotosGatos = async () => {
+    try {
+      const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=5');
+      const json = await response.json();
+      setData((prevData) => [...prevData, ...json]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Fotos de Gatos</Text>
-      <Button title="Carregar fotos"/>
+      <Button title="Carregar 5 fotos de gatos" onPress={obterFotosGatos}/>
     </View>
   );
 }
